9e5e85ab594a785ed3982542d277bc9bb4f506e2
Zaher <zaher@nadrus.com>
2015-10-05 13:45:56 +0300

    -Adding Selenuim Builder scripts : doLogin_HOME -Adding SharedAction Class

--- Nadrus-Selenium-Tests/src/test/java/com/nadrus/SauceTest03.java
+++ Nadrus-Selenium-Tests/src/test/java/com/nadrus/SauceTest03.java
@@ -1,7 +1,7 @@
 package com.nadrus;
-import common.Config;
-
-import com.saucelabs.common.SauceOnDemandAuthentication;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
@@ -11,26 +11,17 @@ import org.junit.rules.TestName;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import com.saucelabs.common.SauceOnDemandAuthentication;
+import com.saucelabs.common.SauceOnDemandSessionIdProvider;
 import com.saucelabs.junit.ConcurrentParameterized;
 import com.saucelabs.junit.SauceOnDemandTestWatcher;
 
 import common.Config;
-
-import java.net.URL;
-import java.util.LinkedList;
-import java.util.concurrent.TimeUnit;
-
-import static org.junit.Assert.*;
-
-import com.saucelabs.common.SauceOnDemandSessionIdProvider;
-
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import common.SharedActions;
 
 
 /**
@@ -92,7 +83,9 @@ public class SauceTest03 implements SauceOnDemandSessionIdProvider {
     /**
      * The {@link WebDriver} instance which is used to perform browser interactions with.
      */
-    private WebDriver driver;
+    private WebDriver wd;
+
+	private SharedActions actions;
 
     /**
      * Constructs a new instance of the test.  The constructor requires three string parameters, which represent the operating
@@ -152,77 +145,102 @@ public class SauceTest03 implements SauceOnDemandSessionIdProvider {
         String methodName = name.getMethodName();
         capabilities.setCapability("name", methodName);
 
-        this.driver = new RemoteWebDriver(
+        this.wd = new RemoteWebDriver(
                 new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() +
                         "@ondemand.saucelabs.com:80/wd/hub"),
                 capabilities);
-        this.driver.manage().timeouts().implicitlyWait(Config.TIMEOUT, TimeUnit.SECONDS);
+        this.wd.manage().timeouts().implicitlyWait(Config.TIMEOUT, TimeUnit.SECONDS);
         
-        this.sessionId = (((RemoteWebDriver) driver).getSessionId()).toString();
+        this.sessionId = (((RemoteWebDriver) wd).getSessionId()).toString();
 
         String message = String.format("SauceOnDemandSessionID=%1$s job-name=%2$s", this.sessionId, methodName);
         System.out.println(message);
+        actions = new SharedActions(this.wd);
     }
 
     /**
-     * Runs a simple test verifying the UI and title of the belk.com home page.
+     * #03 Majhool wants to find a course by keyword
+     * https://leantesting.com/en/projects/7121/test-plan/3056/edit
+     * 
      * @throws Exception
      */
     @Test
-    public void runTestID03_1() throws Exception {
-    	driver.get(Config.startURL+"/theme/1");
-        driver.findElement(By.xpath("//div[5]/div[1]/div/a")).click();
-        driver.findElement(By.id("CourseTitles")).click();
-        driver.findElement(By.id("CourseTitles")).clear();
-        driver.findElement(By.id("CourseTitles")).sendKeys("مايكروسوفت");
-        driver.findElement(By.xpath("//form[@id='CourseSearchForm']/div/div/button")).click();
-        if (!driver.findElement(By.tagName("html")).getText().contains("Windows")) {
-            System.out.println("verifyTextPresent failed");
-        }
-        if (!driver.findElement(By.tagName("html")).getText().contains("Office")) {
-            System.out.println("verifyTextPresent failed");
-        }
-        if (!driver.findElement(By.tagName("html")).getText().contains("Skype")) {
-            System.out.println("verifyTextPresent failed");
-        }
+    public void runTestID03_Majhool() throws Exception {
+		  wd.get(Config.startURL+"/theme/1");
+	  	  wd.get(Config.startURL+"/view-courses");
+	  	  runTestID03();
     }
     
     @Test
-    public void runTestID03_2() throws Exception {
-    	driver.get(Config.startURL+"/theme/1");
-        driver.findElement(By.xpath("//div[5]/div[1]/div/a")).click();
-        driver.findElement(By.id("CourseTitles")).click();
-        driver.findElement(By.id("CourseTitles")).clear();
-        driver.findElement(By.id("CourseTitles")).sendKeys("اندرويد");
-        driver.findElement(By.xpath("//form[@id='CourseSearchForm']/div/div/button")).click();   
+    public void runTestID03_Dakhil() throws Exception {
+    	actions.doLogin("dakhil@freeletter.me", "123456", SharedActions.doLogin_HOME);
+    	runTestID03();
     }
     
-    @Test
-    public void runTestID03_3() throws Exception {
-        driver.get(Config.startURL+"/theme/1");
-        driver.findElement(By.xpath("//div[5]/div[1]/div/a")).click();
-        driver.findElement(By.id("CourseTitles")).click();
-        driver.findElement(By.id("CourseTitles")).clear();
-        driver.findElement(By.id("CourseTitles")).sendKeys("الاندرويد");
-        driver.findElement(By.xpath("//form[@id='CourseSearchForm']/div/div/button")).click();
-        if (!driver.findElement(By.tagName("html")).getText().contains("تعلم برمجة الأندرويد من الصفر حتى الأحتراف")) {
-            System.out.println("verifyTextPresent failed");
-        }
-    }    
+    public void runTestID03(){
+  	  wd.findElement(By.id("CourseTitles")).click();
+  	  wd.findElement(By.id("CourseTitles")).clear();
+  	  wd.findElement(By.id("CourseTitles")).sendKeys("مايكروسوفت");
+  	  wd.findElement(By.cssSelector("button.btn.btn-search")).click();
 
-    @Test
-    public void runTestID03_4() throws Exception {
-        driver.get(Config.startURL+"/theme/1");
-        driver.findElement(By.xpath("//div[5]/div[1]/div/a")).click();
-        driver.findElement(By.id("CourseTitles")).click();
-        driver.findElement(By.id("CourseTitles")).clear();
-        driver.findElement(By.id("CourseTitles")).sendKeys("أندرويد");
-        driver.findElement(By.xpath("//form[@id='CourseSearchForm']/div/div/button")).click();
-        if (!driver.findElement(By.tagName("html")).getText().contains("تعلم برمجة الأندرويد من الصفر حتى الأحتراف")) {
-            System.out.println("verifyTextPresent failed");
-        }
+  	  if (!wd.findElement(By.tagName("html")).getText().contains("برمجة قواعد البيانات باستخدام SQL Server للمطورين المبتدئين")) {
+  		  System.out.println("verifyTextPresent failed");
+  	  }
+  	  if (!wd.findElement(By.tagName("html")).getText().contains("مدخل الى مايكروسوفت إكسل Office Excel 2013")) {
+  		  System.out.println("verifyTextPresent failed");
+  	  }
+  	  if (!wd.findElement(By.tagName("html")).getText().contains("الدورة المتكاملة لاحتراف MS Office Word 2013")) {
+  		  System.out.println("verifyTextPresent failed");
+  	  }
+  	  if (!wd.findElement(By.tagName("html")).getText().contains("أساسيات ويندوز 8 Windows Essentials")) {
+  		  System.out.println("verifyTextPresent failed");
+  	  }
+  	  if (!wd.findElement(By.tagName("html")).getText().contains("المهارات المتقدمة في Windows 8")) {
+  		  System.out.println("verifyTextPresent failed");
+  	  }
+  	  if (!wd.findElement(By.tagName("html")).getText().contains("مقدمة في باتش -Batch")) {
+  		  System.out.println("verifyTextPresent failed");
+  	  }
+  	  if (!wd.findElement(By.tagName("html")).getText().contains("أساسيات OneNote 2013")) {
+  		  System.out.println("verifyTextPresent failed");
+  	  }
+  	  wd.findElement(By.id("CourseTitles")).click();
+  	  wd.findElement(By.id("CourseTitles")).clear();
+  	  wd.findElement(By.id("CourseTitles")).sendKeys("اندرويد");
+  	  wd.findElement(By.cssSelector("button.btn.btn-search")).click();
+  	  if (!wd.findElement(By.tagName("html")).getText().contains("تعلم لغة الجافا خطوة بخطوة")) {
+  		  System.out.println("verifyTextPresent failed");
+  	  }
+  	  if (!wd.findElement(By.tagName("html")).getText().contains("تعلم برمجة الأندرويد من الصفر حتى الأحتراف")) {
+  		  System.out.println("verifyTextPresent failed");
+  	  }
+  	  if (!wd.findElement(By.tagName("html")).getText().contains("صيانة الاجهزة حسب اختبار 220-802 لشهادة A+ من CompTIA")) {
+  		  System.out.println("verifyTextPresent failed");
+  	  }
+  	  wd.findElement(By.id("CourseTitles")).click();
+  	  wd.findElement(By.id("CourseTitles")).clear();
+  	  wd.findElement(By.id("CourseTitles")).sendKeys("الاندرويد");
+  	  wd.findElement(By.cssSelector("button.btn.btn-search")).click();
+  	  if (!wd.findElement(By.tagName("html")).getText().contains("تعلم لغة الجافا خطوة بخطوة")) {
+  	  System.out.println("verifyTextPresent failed");
+  	  }
+  	  if (!wd.findElement(By.tagName("html")).getText().contains("تعلم برمجة الأندرويد من الصفر حتى الأحتراف")) {
+  	  System.out.println("verifyTextPresent failed");
+  	  }
+  	  if (!wd.findElement(By.tagName("html")).getText().contains("صيانة الاجهزة حسب اختبار 220-802 لشهادة A+ من CompTIA")) {
+  	  System.out.println("verifyTextPresent failed");
+  	  }
+  	  wd.findElement(By.id("CourseTitles")).click();
+  	  wd.findElement(By.id("CourseTitles")).clear();
+  	  wd.findElement(By.id("CourseTitles")).sendKeys("أندرويد");
+  	  wd.findElement(By.cssSelector("button.btn.btn-search")).click();
+  	  if (!wd.findElement(By.tagName("html")).getText().contains("تعلم لغة الجافا خطوة بخطوة")) {
+  	  System.out.println("verifyTextPresent failed");
+  	  }
+  	  if (!wd.findElement(By.tagName("html")).getText().contains("تعلم برمجة الأندرويد من الصفر حتى الأحتراف")) {
+  	  System.out.println("verifyTextPresent failed");
+  	  }
     }
-
     /**
      * Closes the {@link WebDriver} session.
      *
@@ -230,7 +248,7 @@ public class SauceTest03 implements SauceOnDemandSessionIdProvider {
      */
     @After
     public void tearDown() throws Exception {
-        driver.quit();
+        wd.quit();
     }
 
     /**
